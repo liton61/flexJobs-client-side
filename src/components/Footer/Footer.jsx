e444d194650b89b0e76705edaa49b0c46316f896
liton61 <litonofficial61@gmail.com>
2023-11-05 00:23:26 +0600

    footer added

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,9 +1,39 @@
-
+import logo from '../../assets/logo (1).png';
 
 const Footer = () => {
     return (
         <div>
-            <h1>Footer</h1>
+            <footer className="footer p-10 bg-base-200 text-base-content">
+                <aside>
+                    <img className='w-28' src={logo} alt="" />
+                    <h3>Copyright © 2023 - All right reserved</h3>
+                    <div className="grid grid-flow-col gap-4 text-2xl cursor-pointer">
+                        <i className="fa-brands fa-facebook"></i>
+                        <i className="fa-brands fa-twitter"></i>
+                        <i className="fa-brands fa-instagram"></i>
+                        <i className="fa-brands fa-linkedin"></i>
+                        <i className="fa-brands fa-youtube"></i>
+                    </div>
+                </aside>
+                <nav>
+                    <header className="footer-title">Services</header>
+                    <a className="link link-hover">Branding</a>
+                    <a className="link link-hover">Design</a>
+                    <a className="link link-hover">Marketing</a>
+                </nav>
+                <nav>
+                    <header className="footer-title">Company</header>
+                    <a className="link link-hover">About us</a>
+                    <a className="link link-hover">Contact</a>
+                    <a className="link link-hover">Jobs</a>
+                </nav>
+                <nav>
+                    <header className="footer-title">Legal</header>
+                    <a className="link link-hover">Terms of use</a>
+                    <a className="link link-hover">Privacy policy</a>
+                    <a className="link link-hover">Cookie policy</a>
+                </nav>
+            </footer>
         </div>
     );
 };
