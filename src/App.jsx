c4e6d5aada0e1cf7c647645c3ee1aae57fbb9202
liton61 <litonofficial61@gmail.com>
2023-11-04 20:31:53 +0600

    navbar added

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Flex Jobs</h1>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,69 @@
+import { NavLink } from "react-router-dom";
+
+const Header = () => {
+    return (
+        <div>
+            <div className="navbar bg-green-300">
+                <div className="navbar-start">
+                    <div className="dropdown">
+                        <label tabIndex={0} className="btn btn-ghost lg:hidden">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-6 h-6 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
+                        </label>
+                        <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+                            <li><NavLink
+                                to="/"
+                                className={({ isActive, isPending }) =>
+                                    isPending ? "pending" : isActive ? "active" : ""}>Home</NavLink></li>
+                            <li><NavLink
+                                to="/addJob"
+                                className={({ isActive, isPending }) =>
+                                    isPending ? "pending" : isActive ? "active" : ""}>Add Job</NavLink></li>
+                            <li><NavLink
+                                to="/myPostedJob"
+                                className={({ isActive, isPending }) =>
+                                    isPending ? "pending" : isActive ? "active" : ""}>My Posted Jobs</NavLink></li>
+                            <li><NavLink
+                                to="/myBides"
+                                className={({ isActive, isPending }) =>
+                                    isPending ? "pending" : isActive ? "active" : ""}>My Bids</NavLink></li>
+                            <li><NavLink
+                                to="/bideRequests"
+                                className={({ isActive, isPending }) =>
+                                    isPending ? "pending" : isActive ? "active" : ""}>Bid Requests</NavLink></li>
+                        </ul>
+                    </div>
+                    <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
+                </div>
+                <div className="navbar-center hidden lg:flex">
+                    <ul className="menu menu-horizontal px-1">
+                        <li><NavLink
+                            to="/"
+                            className={({ isActive, isPending }) =>
+                                isPending ? "pending" : isActive ? "active" : ""}>Home</NavLink></li>
+                        <li><NavLink
+                            to="/addJob"
+                            className={({ isActive, isPending }) =>
+                                isPending ? "pending" : isActive ? "active" : ""}>Add Job</NavLink></li>
+                        <li><NavLink
+                            to="/myPostedJob"
+                            className={({ isActive, isPending }) =>
+                                isPending ? "pending" : isActive ? "active" : ""}>My Posted Jobs</NavLink></li>
+                        <li><NavLink
+                            to="/myBides"
+                            className={({ isActive, isPending }) =>
+                                isPending ? "pending" : isActive ? "active" : ""}>My Bids</NavLink></li>
+                        <li><NavLink
+                            to="/bideRequests"
+                            className={({ isActive, isPending }) =>
+                                isPending ? "pending" : isActive ? "active" : ""}>Bid Requests</NavLink></li>
+                    </ul>
+                </div>
+                <div className="navbar-end">
+                    <a className="btn">Button</a>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Header;
